6ebfe10467996af1fb9a48a7b1c8c8317d9238c7
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2024-11-11 09:40:27 +0900

    Change method name

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import uno
 
-def my_first_macro_calc(*args):
+def hello_world(*args):
     doc = XSCRIPTCONTEXT.getDocument()
     cell = doc.Sheets[0]['A1']  # com.sun.star.sheet.XSpreadsheetDocument
     cell.setString('PythonからCalcでHello World')
